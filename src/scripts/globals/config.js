819a55cd5034c727ea28a2644bee8f5424a64abe
Aldi Milano <milano.aldi@gmail.com>
2022-12-28 19:11:40 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '1d29e799bb3728c10ef98df0f4ed823f',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
